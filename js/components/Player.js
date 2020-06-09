a24bd1ddcce67f00f6fd73ee1f681845937802d1
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-09 16:19:55 +0100

    feat: Add player movement methods

--- js/components/Player.js
+++ js/components/Player.js
@@ -5,6 +5,34 @@ class Player extends Person {
     super(name, xCoord, yCoord, color);
     this.color = '#5BFFFF';
   }
+
+  get xPosition() {
+    return this.xCoord;
+  }
+
+  get yPosition() {
+    return this.yCoord;
+  }
+
+  moveToken(direction) {
+    switch (direction) {
+      case 'up':
+        this.token.animate(100, 10, 'now').dy(-30);
+        break;
+      case 'left':
+        this.token.animate(100, 10, 'now').dx(-30);
+        break;
+      case 'down':
+        this.token.animate(100, 10, 'now').dy(30);
+        break;
+      case 'right':
+        this.token.animate(100, 10, 'now').dx(30);
+        break;
+      // no default
+    }
+    this.xCoord = this.token.cx();
+    this.yCoord = this.token.cy();
+  }
 }
 
 export default Player;
