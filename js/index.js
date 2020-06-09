f68bdf21d3de4fc9b0d8344a106de6a10311c1be
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-09 11:42:20 +0100

    feat: Draw enemy tokens

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 import gameBoard from '../js/components/game-board.js';
+import tokens from '../js/components/tokens.js'
 
 window.onload = () => {
   gameBoard
@@ -6,4 +7,8 @@ window.onload = () => {
     .drawBoard()
     .drawHome()
     .drawSupermarket();
+
+  tokens.forEach((token) => {
+    token.drawToken();
+  });
 };
