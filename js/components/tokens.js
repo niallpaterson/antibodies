9fd04292025de287b6a70a01478243070a1e4f6e
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-10 13:06:45 +0100

    feat: Use npc class for npc tokens

--- js/components/tokens.js
+++ js/components/tokens.js
@@ -1,20 +1,20 @@
-import Person from './Person.js';
+import Npc from './Npc.js';
 import Player from './Player.js'
 
 const tokens = {
   npcs: [
-    new Person('geraldine', 62, 43),
-    new Person('keith', 125, 54),
-    new Person('dorothy', 175, 35),
-    new Person('jezebel', 117, 87),
-    new Person('baloo', 26, 158),
-    new Person('sheila', 90, 173),
-    new Person('dotty', 260, 100),
-    new Person('ollie', 236, 158),
-    new Person('ethan', 190, 253),
-    new Person('genie', 205, 202),
-    new Person('matt', 117, 268),
-    new Person('shea', 70, 80),
+    new Npc('geraldine', 62, 43),
+    new Npc('keith', 125, 54),
+    new Npc('dorothy', 175, 35),
+    new Npc('jezebel', 117, 87),
+    new Npc('baloo', 26, 158),
+    new Npc('sheila', 90, 173),
+    new Npc('dotty', 260, 100),
+    new Npc('ollie', 236, 158),
+    new Npc('ethan', 190, 253),
+    new Npc('genie', 205, 202),
+    new Npc('matt', 117, 268),
+    new Npc('shea', 70, 80),
   ],
   player: new Player('player', 25, 275),
 };
