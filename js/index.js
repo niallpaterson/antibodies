cef26d839b5488cf6e735db03e6ed4e449f5dace
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-10 16:41:48 +0100

    fix: Npc movement interval not run on load

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 import addRunGameEventListeners from './components/run-game-listeners.js';
 import runPlayerMovementIntervals from './components/player-movement.js';
+import runNpcMovementIntervals from './components/npc-movement.js';
 
 window.onload = () => {
   addRunGameEventListeners
     .toStartButton();
   runPlayerMovementIntervals();
+  runNpcMovementIntervals();
 };
