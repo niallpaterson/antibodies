b5542a9fe9e6cb05b4b84ed04e42b9ca4b76a24f
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-10 15:21:18 +0100

    fix: Player movement does not reset on replay

--- js/components/reset-game.js
+++ js/components/reset-game.js
@@ -1,8 +1,13 @@
 import startGame from './start-game.js';
 import gameBoard from './game-board.js';
+import settings from './settings.js';
 
 const resetGame = () => {
   gameBoard.svgFrame.remove();
+  settings.playerMoveRight = false;
+  settings.playerMoveLeft = false;
+  settings.playerMoveUp = false;
+  settings.playerMoveDown = false;
   startGame();
 };
 
