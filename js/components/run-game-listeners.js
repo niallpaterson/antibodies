83a5bcae3771e649c549dd8f5ee2d55f1e58e085
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-11 19:59:24 +0100

    fix: Start button works only first time
    Also fixed strange bug (issue #43)

--- js/components/run-game-listeners.js
+++ js/components/run-game-listeners.js
@@ -1,10 +1,20 @@
 import startGame from './start-game.js';
+import resetGame from './reset-game.js';
 
 const addRunGameEventListeners = {
+  gameIsRunning: false,
   toStartButton() {
     document
       .querySelector('#start-btn')
-      .addEventListener('click', () => { startGame(); });
+      .addEventListener('click', () => {
+        if (!this.gameIsRunning) {
+          startGame();
+          this.gameIsRunning = true;
+        } else {
+          resetGame();
+          startGame();
+        }
+      });
   },
 };
 
