ef8510582add9c7b95b861923ba19af78de666ba
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-11 19:19:21 +0100

    chore: Update import

--- js/components/start-game.js
+++ js/components/start-game.js
@@ -1,4 +1,4 @@
-import addEventListeners from './key-events.js';
+import addEventListeners from './key-event-listeners.js';
 import npcIntervals from './npc-movement.js';
 import tokens from './tokens.js';
 import collisionIntervals from './collisions.js';
