e4583f61a629f3cb2f42fd5d907d9eb6f786b4bb
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-11 19:22:48 +0100

    refactor: Tidy reset game function

--- js/components/reset-game.js
+++ js/components/reset-game.js
@@ -1,28 +1,24 @@
-import startGame from './start-game.js';
 import tokens from './tokens.js';
 import scoreCounter from './score-counter.js';
 
 const resetGame = () => {
+  tokens.npcCounter = 0;
   tokens.npcs.forEach((npc) => {
-    console.log(tokens.npcs)
-    npc.resetCoordinates().resetInfection();
-    npc.token.remove();
+    npc
+      .resetCoordinates()
+      .resetInfection()
+      .removeToken();
   });
 
-  // messy! Refactor plz
-  for (let i = 0; i <= tokens.npcCounter; i++) {
-    tokens.inactiveNpcs.unshift(tokens.npcs.pop());
-  }
-
-  tokens.npcCounter = 0;
-
   tokens.player.token.remove();
+  tokens.player.hasShopping = false;
   tokens.player.moveUp = false;
   tokens.player.moveDown = false;
   tokens.player.moveRight = false;
   tokens.player.moveLeft = false;
   tokens.player.isInfected = false;
-  scoreCounter.reset();  
+
+  scoreCounter.reset();
 };
 
 export default resetGame;
