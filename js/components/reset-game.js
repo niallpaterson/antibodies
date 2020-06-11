a906f15df7f8dbf8981c1862a9374fe299f4aa23
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-11 18:01:59 +0100

    feat: Add token resets

--- js/components/reset-game.js
+++ js/components/reset-game.js
@@ -4,16 +4,25 @@ import scoreCounter from './score-counter.js';
 
 const resetGame = () => {
   tokens.npcs.forEach((npc) => {
+    console.log(tokens.npcs)
+    npc.resetCoordinates().resetInfection();
     npc.token.remove();
   });
-  tokens.player.token.remove();
 
+  // messy! Refactor plz
+  for (let i = 0; i <= tokens.npcCounter; i++) {
+    tokens.inactiveNpcs.unshift(tokens.npcs.pop());
+  }
+
+  tokens.npcCounter = 0;
+
+  tokens.player.token.remove();
   tokens.player.moveUp = false;
   tokens.player.moveDown = false;
   tokens.player.moveRight = false;
   tokens.player.moveLeft = false;
-  scoreCounter.reset();
-  startGame();
+  tokens.player.isInfected = false;
+  scoreCounter.reset();  
 };
 
 export default resetGame;
