7afbb9c6e7e0441623fe18f796f817d272b985fc
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-11 20:35:15 +0100

    feat: Restyle UI

--- js/components/score-counter.js
+++ js/components/score-counter.js
@@ -2,11 +2,11 @@ const scoreCounter = {
   score: 0,
   increment() {
     this.score += 1;
-    document.querySelector('#score-counter').textContent = this.score;
+    document.querySelector('#score-counter').textContent = `Shopping Trips: ${this.score}`;
   },
   reset() {
     this.score = 0;
-    document.querySelector('#score-counter').textContent = '0';
+    document.querySelector('#score-counter').textContent = 'Shopping Trips: 0';
   },
 };
 
